f6ec8011944b8e889b5e17e142e8ed64cbf97ce1
Taqim22 <mustaqimsidebang@students.usu.ac.id>
2024-04-19 08:34:55 +0700

    first commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Sidebang</title>
+    <link href="{{ asset('sb-admin/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    <nav id="navbar-samping">
+        <div class="row justify-content-center fixed-top">
+            <div class="col-xs-8">
+                <div class="card shadow-sm border-0 rounded-lg mt-3 bg-info">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house text-light"></i></a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" id="navbarDropdownMenuLink" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Produk</a>
+                            <ul class="dropdown-menu bg-info" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item text-light" href="#">Baba</a></li>
+                                <li><a class="dropdown-item text-light" href="#">Ternak</a></li>
+                                <li><a class="dropdown-item text-light" href="#">Aplikasi</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Portofolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">Blog</a>
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-2">
+                <div class="card shadow-sm border-0 rounded-lg mt-3 ml-2 bg-info">
+                    <ul class="nav justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link active" class="button" aria-current="page" href="#"><i class="fa-solid fa-right-to-bracket text-light"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <section class="jumbotron text-center">
+            <img src="{{asset('logo/logomasa.png')}}" alt="" width="200" class="rounded-circle img-thumbnail mt-2">
+            <h1 class="display-4">Sidebang Group</h1>
+            <p class="lead">Usaha keluarga yang menawarkan produk berkualitas, pelayanan baik, dan mengutamakan kepercayaan pelanggan.</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L40,117.3C80,139,160,181,240,170.7C320,160,400,96,480,85.3C560,75,640,117,720,122.7C800,128,880,96,960,80C1040,64,1120,64,1200,80C1280,96,1360,128,1400,144L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+    <section id="produkDigital">
+        <div class="container">
+            <h3 class="text-center mb-4">Produk Digital</h3>
+            <div class="row text-center justify-content-center">
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8bffec" fill-opacity="1" d="M0,64L40,96C80,128,160,192,240,202.7C320,213,400,171,480,170.7C560,171,640,213,720,213.3C800,213,880,171,960,128C1040,85,1120,43,1200,37.3C1280,32,1360,64,1400,80L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+    
+    <section id="dagang">
+        <div class="container">
+            <h3 class="text-center mb-4">Produk Dagang</h3>
+            <div class="row text-center justify-content-center">
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,288L40,240C80,192,160,96,240,96C320,96,400,192,480,192C560,192,640,96,720,90.7C800,85,880,171,960,213.3C1040,256,1120,256,1200,245.3C1280,235,1360,213,1400,202.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+
+    <section id="ternak">
+        <div class="container">
+            <h3 class="text-center mb-4">Produk Ternak</h3>
+            <div class="row text-center justify-content-center">
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="card">
+                        <img src="{{asset('foto/1.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#58ffd5" fill-opacity="1" d="M0,64L40,85.3C80,107,160,149,240,154.7C320,160,400,128,480,112C560,96,640,96,720,101.3C800,107,880,117,960,144C1040,171,1120,213,1200,208C1280,203,1360,149,1400,122.7L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+
+    <section id="blog">
+        <h3 class="text-center">BLOG</h3>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,256L40,256C80,256,160,256,240,250.7C320,245,400,235,480,224C560,213,640,203,720,170.7C800,139,880,85,960,58.7C1040,32,1120,32,1200,58.7C1280,85,1360,139,1400,165.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+    </section>
+
+    <section id="kontak">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                <form action="#" class="mb-4">
+                    <h3 class="text-center">Hubungi Kami</h3>
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="email" class="form-control" id="nama" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Alamat email</label>
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="textEmail" class="form-label">Pesan anda</label>
+                        <textarea class="form-control" id="textEmail" rows="3"></textarea>
+                    </div>
+                    <button class="btn btn-primary">Kirim Pesan</button>
+                </form>    
+                    
+                </div>
+                <div class="col-md-3 mx-auto">
+                <img src="{{asset('foto/rumah_sidebang.svg')}}" alt="Deskripsi" style="width: 110%; height: auto;">
+                </div>
+            </div>
+        </div>
+    </section>
+            
+    <section id="footer">
+        <footer class="py-4 bg-dark mt-auto">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 justify-content-start text-light">
+                        <h4>Kontak</h4>
+                        <p><i class="fa-brands fa-whatsapp"></i> No.HP/WA: <a href="http://wa.me/+6285361513541" class="text-white" target="_blank">085361513541</a></p>
+                        <p><i class="fa-brands fa-instagram"></i> Instagram: <a href="http://instagram.com/taqim_sidebang" class="text-white" target="_blank">taqim_sidebang</a></p>
+                        <p><i class="fa-regular fa-envelope"></i> Email: <a href="mailto:taqimtech@gmail.com?subject=Pesan Sidebang.id&body=Halo, saya menemukan anda di sidebang.id, ingin mengirim pesan kepada Anda." class="text-white">taqimtech@gmail.com</a></p>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4 justify-content-end text-light">
+                        <h4>Produk</h4>
+                        <p><a href="#">Produk Digital</a></p>
+                        <p><a href="#">Produk Dagang</a></p>
+                    </div>
+                </div>
+                    <hr>
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-light">Copyright &copy; Taqims</div>
+                    <div>
+                        <a class="text-light">Sidebang Group</a>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{asset('sb-admin/js/scripts.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+
+
+    
+
+
+
+</body>
+
+</html>
